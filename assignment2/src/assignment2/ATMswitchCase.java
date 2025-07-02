fda15b970a37831f0b7f8f12602e327250921e89
abhilasha-tugave <tugaveabhilasha9560@gmail.com>
2025-07-02 23:10:53 +0530

    second assignment

---
+++ assignment2/src/assignment2/ATMswitchCase.java
@@ -0,0 +1,33 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class ATMswitchCase {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Select 1 to check balance | 2 to deposit | 3 to withdraw money | 4 to Quit");
+		System.out.println("Select an one option ");
+		int option = sc.nextInt();
+		
+		switch(option) {
+		case 1:
+			System.out.println("Checking balance");
+			break;
+		case 2:
+			System.out.println("You selected deposit option");
+			break;
+		case 3:
+			System.out.println("You selected withdraw money option");
+			break;
+		case 4:
+			System.out.println("You selected quit option");
+			break;
+		default:
+			System.out.println("Invalid option");
+		}
+		
+		sc.close();
+	}
+
+}
